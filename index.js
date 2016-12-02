0bf3192a6f83e82bb36356e40f78baced10aeba0
lastuniverse <kapa6ac@inbox.ru>
2016-12-03 01:16:29 +0300

    add messanger

--- index.js
+++ index.js
@@ -6,8 +6,61 @@
  * MIT Licensed
  */
 
-var LFB = {};
+var messanger = require('./lib/Messanger');
 
-LFB.Server = require('./lib/LFBserver');
+var server = new messanger({port: 3000});
 
-var server = new LFB.Server({port: 3000});
+// var Server = require('./lib/LFBserver');
+
+// var LFB = {};
+
+// LFB.Server = Server;
+
+// var server = new LFB.Server({port: 3000});
+
+
+
+
+var WebSocket = require('ws');
+var ws1 = new WebSocket('ws://localhost:3000/', {
+  protocolVersion: 8,
+  origin: 'http://localhost:3000/'
+});
+
+ws1.on('open', function open() {
+  //console.log('connected');
+  ws1.send(
+    JSON.stringify({name: "__system__",data: {test: "blablabla1"}})
+  );
+  ws1.send(
+    JSON.stringify({name: "12345",data: {test: "blablabla2"}})
+  );
+  ws1.send(JSON.stringify("blablabla3"));
+
+});
+
+ws1.on('close', function close() {
+  //console.log('disconnected');
+});
+
+ws1.on('message', function (data, flags) {
+  console.log('ws1 get message: ', data);
+});
+
+var ws2 = new WebSocket('ws://localhost:3000/', {
+  protocolVersion: 8,
+  origin: 'http://localhost:3000/'
+});
+
+ws2.on('open', function open() {
+  // console.log('connected');
+  // ws2.send("ws2.send1", {mask: true});
+});
+
+ws2.on('close', function close() {
+  // console.log('disconnected');
+});
+
+ws2.on('message', function (data, flags) {
+  console.log('ws2 get message: ', data);
+});
